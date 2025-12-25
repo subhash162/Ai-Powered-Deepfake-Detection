6b1363c6df6b4ffcf36e958f8d212f603de8d2a9
Apple <apple@Apples-MacBook-Pro.local>
2025-12-26 02:48:21 +0545

    minor adjustments

--- alembic/env.py
+++ alembic/env.py
@@ -18,15 +18,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.db.base import Base  # or wherever Base is defined
+from app.db.base import Base  
 target_metadata = Base.metadata
-
-# target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
